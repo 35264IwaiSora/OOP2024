5b74ec9a5cacb555d9dba6e15bd0541a5ff3e629
IwaiSora <166583838+35264IwaiSora@users.noreply.github.com>
2024-04-18 15:01:26 +0900

    距離換算プログラム完成

--- Chapter02/DistanceConverter/FeetConverter.cs
+++ Chapter02/DistanceConverter/FeetConverter.cs
@@ -5,14 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DistanceConverter {
-    public static class FeetConverter {        
+    public static class FeetConverter {
+
+        //private const double raito = 0.3048; //定数
+
+        public static readonly double raito = 0.3048; //定数（外部にも公開する場合）
+
         //メートルからフィートを求める
         public static double FromMeter(double meter) {
-            return meter / 0.3048;
+            return meter / raito;
         }
         //フィートからメートルを求める
         public static double ToMeter(double feet) {
-            return feet * 0.3048;
+            return feet * raito;
         }
     }
 }
