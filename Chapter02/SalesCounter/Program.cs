eec498f48381c2bf2117882816619849e23cf05b
IwaiSora <166583838+35264IwaiSora@users.noreply.github.com>
2024-04-24 14:08:45 +0900

    メソッドの移動

--- Chapter02/SalesCounter/Program.cs
+++ Chapter02/SalesCounter/Program.cs
@@ -8,31 +8,13 @@ using System.Threading.Tasks;
 namespace SalesCounter {
     internal class Program {
         static void Main(string[] args) {
-            //戻り値を受け取る
-            List<Sale> sales = ReadSales(@"C:\Users\infosys\source\repos\OOP2024\Chapter02\SalesCounter\bin\Debug\data\Sales.csv");
-
-            //戻り値のコレクションを一件ずつ出力
-            foreach (Sale sale in sales) {
-                //Console.WriteLine( sale.ShopName +" "+ sale.ProductCategory +" "+ sale.Amount);
-                //Console.WriteLine("店名：{0} カテゴリー：{1} 売上：{2}",sale.ShopName,sale.ProductCategory,sale.Amount);  
-                Console.WriteLine($"店名：{sale.ShopName} カテゴリー：{sale.ProductCategory} 売上：{sale.Amount}");
+            SalesCounter sales = new SalesCounter(@"data\sales.csv");
+            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
+            foreach (KeyValuePair<string, int> obj in amountPerStore) {
+                Console.WriteLine("{0}{1}", obj.Key, obj.Value);
             }
-        }
-
-        //売上データを読み込みSaleオブジェクトを返す
-        static List<Sale> ReadSales(string filePath) {
-            List<Sale> sales = new List<Sale>();
-            string[] lines = File.ReadAllLines(filePath);
-            foreach (string line in lines) {
-                string[] items = line.Split(',');
-                Sale sale = new Sale {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2]),
-                };
-                sales.Add(sale);
-            }
-            return sales;
         }
     }
 }
+
+
---
+++ Chapter02/SalesCounter/SalesCounter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCounter {
+    public class SalesCounter {
+        private List<Sale> _sales;
+
+        //コンストラクタ
+        public SalesCounter(string filePass) {
+            _sales = ReadSales(filePass);
+        }
+
+        //売上データを読み込みSaleオブジェクトを返す
+        private static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2]),
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+
+        //店舗別の売り上げを求める
+        public Dictionary<string, int> GetPerStoreSales() {
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Sale sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName)) {
+                    dict[sale.ShopName] += sale.Amount;
+                } else {
+                    dict[sale.ShopName] = sale.Amount;
+                }
+            }
+            return dict;
+        }
+    }
+}
