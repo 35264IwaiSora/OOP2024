ce73a672fc4f1fe071ac52a5d0770577c9245524
IwaiSora <166583838+35264IwaiSora@users.noreply.github.com>
2024-04-24 14:49:30 +0900

    インターフェイスの実装

--- Chapter02/SalesCounter/Program.cs
+++ Chapter02/SalesCounter/Program.cs
@@ -9,7 +9,7 @@ namespace SalesCounter {
     internal class Program {
         static void Main(string[] args) {
             SalesCounter sales = new SalesCounter(@"data\sales.csv");
-            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
+            IDictionary<string, int> amountPerStore = sales.GetPerStoreSales();
             foreach (KeyValuePair<string, int> obj in amountPerStore) {
                 Console.WriteLine("{0}{1}", obj.Key, obj.Value);
             }
