ab84f5af4cd309b76b7ffaf1eb036906a76ac02f
IwaiSora <166583838+35264IwaiSora@users.noreply.github.com>
2024-05-08 15:59:53 +0900

    サッカーボールとテニスボールの複数同時移動

--- FormApps/BallApp/Form1.cs
+++ FormApps/BallApp/Form1.cs
@@ -2,9 +2,9 @@ using System.Security.Permissions;
 
 namespace BallApp {
     public partial class Form1 : Form {
-        Obj ball;   
-        PictureBox pb;
-       
+        //listコレクション
+        private List<Obj> balls = new List<Obj>();
+        private List<PictureBox> pbs = new List<PictureBox>();
         //コンストラクタ
         public Form1() {
             InitializeComponent();
@@ -14,26 +14,28 @@ namespace BallApp {
         //フォームが最初にロードされるとき一度だけ実行される
         private void Form1_Load(object sender, EventArgs e) {
             //this.BackColor = Color.Green;
-            
+
         }
 
         private void timer1_Tick(object sender, EventArgs e) {
-            
-            ball.Move();
-            pb.Location = new Point((int)ball.PosX, (int)ball.PosY);
-            
+
+
+            for (int i = 0; i < balls.Count; i++) {
+                balls[i].Move();
+                pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY);
+            }
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e) {
 
-            pb = new PictureBox();
-            
+            PictureBox pb = new PictureBox();
+            Obj ball = null;
             if (e.Button == MouseButtons.Left) {
                 pb.Size = new Size(50, 50);
-                ball = new SoccerBall(e.X-25, e.Y-25);
+                ball = new SoccerBall(e.X - 25, e.Y - 25);
 
-            }else if(e.Button == MouseButtons.Right){
-                pb.Size = new Size(25,25);
+            } else if (e.Button == MouseButtons.Right) {
+                pb.Size = new Size(25, 25);
                 ball = new TennisBall(e.X - 12, e.Y - 12);
             }
 
@@ -42,6 +44,9 @@ namespace BallApp {
             pb.SizeMode = PictureBoxSizeMode.StretchImage;
             pb.Parent = this;
             timer1.Start();
+
+            balls.Add(ball);
+            pbs.Add(pb);
         }
     }
 }
