1a47dc8c075bd09fa2b014aeee148a8f7072d8fc
IwaiSora <166583838+35264IwaiSora@users.noreply.github.com>
2024-05-15 11:48:49 +0900

    p87まで

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+
+            //int count = numbers.Count(n => n % 2 == 0);
+            double num = numbers.Where(n => 5 < n).Average();
+            int total = numbers.Where(n => 5 < n).Sum() ;
+            Console.WriteLine(num);
+            Console.WriteLine(total);
+        }
+    }
+}
