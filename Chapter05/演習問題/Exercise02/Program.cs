09f067137e664c9e30343dabf41d669c782b71f8
IwaiSora <166583838+35264IwaiSora@users.noreply.github.com>
2024-05-30 13:48:02 +0900

    演習問題５.２完成

---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var line = Console.ReadLine();
+            int num;
+            if (int.TryParse(line, out num)) {
+                string formattedNumber = num.ToString("N0");
+                Console.WriteLine(formattedNumber);
+            } else {
+                Console.WriteLine("変換に失敗しました");
+            }
+        }
+    }
+}
