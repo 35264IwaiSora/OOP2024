d91e5c29c55f6ce71d88e9474792a3563f52388f
IwaiSora <166583838+35264IwaiSora@users.noreply.github.com>
2024-05-30 14:54:13 +0900

    演習問題5.3完成

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -25,19 +25,27 @@ namespace Exercise03 {
         }
 
         private static void Exercise3_1(string text) {
-        
+            var spaces = text.Count(c => c == ' ');
+           Console.WriteLine("空白数：[0]",spaces);
         }
 
         private static void Exercise3_2(string text) {
-        
+            var replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
         }
 
         private static void Exercise3_3(string text) {
-        
+            string[] words = text.Split(' ');
+            Console.WriteLine("単語の数："+words.Length);
         }
 
         private static void Exercise3_4(string text) {
-        
+            string[] words = text.Split(' ');
+            var a = words.Where(s => s.Length <= 4);
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
         }
 
         private static void Exercise3_5(string text) {
