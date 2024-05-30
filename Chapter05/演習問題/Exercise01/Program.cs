77617bb470793b85d3c8151a5b2e611c069c7eb4
IwaiSora <166583838+35264IwaiSora@users.noreply.github.com>
2024-05-30 13:24:05 +0900

    演習問題5.1完成

--- Chapter05/演習問題/Exercise01/Program.cs
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
+            var s1 = Console.ReadLine();
+            var s2 = Console.ReadLine();
+            if(string.Compare(s1, s2,ignoreCase:true) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
         }
     }
 }
