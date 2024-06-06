8ec8ebd006869fcf5b7ef4b837d92bbf36d19e5b
IwaiSora <166583838+35264IwaiSora@users.noreply.github.com>
2024-06-06 15:59:13 +0900

    県庁所在地5件登録

---
+++ Chapter07/Section01/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Section01 {
+    internal class Employee {
+        public string Name { get; set; }
+        public string Id { get; set; }
+
+        public Employee(string id, string name) {
+            Id = id;
+            Name = name;
+
+        }
+    }
+}
---
+++ Chapter07/Section01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            //var employeeDict = new Dictionary<int, Employee> {
+            //    { 100, new Employee(100, "清水遼久") },
+            //    { 112, new Employee(112, "芹沢洋和") },
+            //    { 125, new Employee(125, "岩瀬奈央子") },
+            //};
+
+            //employeeDict.Add(126, new Employee(126, "庄野和花"));
+
+            //foreach(var item in employeeDict.Keys) {
+            //    Console.WriteLine($"{item}");
+            //}
+            var prefectureOfficeDict = new Dictionary<string, string>();
+
+            String prefecture;
+            String prefectural_office;
+            Console.WriteLine("県庁所在地の登録");
+
+            for (int i = 0; i < 5; i++) {
+                //都道府県の入力
+                Console.Write("都道府県：");
+                prefecture = Console.ReadLine();
+                //県庁所在地の入力
+                Console.Write("県庁所在地：");
+                prefectural_office = Console.ReadLine();
+                
+                prefectureOfficeDict.Add(prefecture,prefectural_office);
+
+            }
+
+            foreach (var item in prefectureOfficeDict) {
+                    Console.WriteLine("{0}の県庁所在地は{1}です。",item.Key,item.Value);
+            }
+
+
+
+            }
+        }
+}
