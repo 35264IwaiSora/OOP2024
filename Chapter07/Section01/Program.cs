75b6373a9ac80f6a222e290cd484646044ca063c
IwaiSora <166583838+35264IwaiSora@users.noreply.github.com>
2024-06-11 15:52:32 +0900

    メソッド追加

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -7,19 +7,22 @@ using System.Xml.Linq;
 
 namespace Section01 {
     internal class Program {
-        static void Main(string[] args) {
-            
-            var prefectureOfficeDict = new Dictionary<string, string>();
+        static Dictionary<string, string> prefectureOfficeDict = new Dictionary<string, string>();
 
-            String prefecture;
-            String prefectural_office;
+        static void Main(string[] args) {
+
+            String prefecture,prefectural_office;
             Console.WriteLine("県庁所在地の登録");
 
-            for (int i = 0; i < 5; i++) {
+            while(true) {
                 //都道府県の入力
                 Console.Write("都道府県：");
                 prefecture = Console.ReadLine();
-  
+
+                if (prefecture == null) {
+                    break;
+                }
+
                 //県庁所在地の入力
                 Console.Write("県庁所在地：");
                 prefectural_office = Console.ReadLine();
@@ -39,23 +42,14 @@ namespace Section01 {
 
             Boolean endFlag = false; //終了フラグ
             while (!endFlag) {
-                Console.WriteLine("*メニュー*");
-                Console.WriteLine("1:一覧表示");
-                Console.WriteLine("2:検索");
-                Console.WriteLine("9:終了");
-                string menuSelect = Console.ReadLine();
-                switch (menuSelect) {
+                
+                switch (menuDisp()) {
                     case "1":
-                        foreach (var item in prefectureOfficeDict) {
-                            Console.WriteLine("{0}の県庁所在地は{1}です。", item.Key, item.Value);
-                        }
+                        listDisplay();
                         break;
 
                     case "2":
-                        Console.Write("都道府県：");
-                        prefecture = Console.ReadLine();
-                        Console.WriteLine(prefecture + "の県庁所在地は" + prefectureOfficeDict[prefecture] + "です。");
-
+                        prefecture = serachPrefectural_office();
                         break;
 
                     case "9":
@@ -65,5 +59,27 @@ namespace Section01 {
             }
         }
 
+        private static string serachPrefectural_office() {
+            string prefecture;
+            Console.Write("都道府県：");
+            prefecture = Console.ReadLine();
+            Console.WriteLine(prefecture + "の県庁所在地は" + prefectureOfficeDict[prefecture] + "です。");
+            return prefecture;
+        }
+        //一覧表示
+        private static void listDisplay() {
+            foreach (var item in prefectureOfficeDict) {
+                Console.WriteLine("{0}の県庁所在地は{1}です。", item.Key, item.Value);
+            }
+        }
+        //メニュー表示
+        private static string menuDisp() {
+            Console.WriteLine("*メニュー*");
+            Console.WriteLine("1:一覧表示");
+            Console.WriteLine("2:検索");
+            Console.WriteLine("9:終了");
+            string menuSelect = Console.ReadLine();
+            return menuSelect;
+        }
     }
 }
