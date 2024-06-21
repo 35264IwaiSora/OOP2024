88be855dae70cb006a7c2584544c9b89ca6997d2
IwaiSora <166583838+35264IwaiSora@users.noreply.github.com>
2024-06-21 10:17:19 +0900

    入力した日付の曜日を出力

---
+++ Chapter08/Section01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("生年月日を入力");
+            Console.Write("年：");
+            var year = int.Parse(Console.ReadLine());
+            Console.Write("月：");
+            var month = int.Parse(Console.ReadLine());
+            Console.Write("日：");
+            var day = int.Parse(Console.ReadLine());
+
+            var birthday = new DateTime(year, month, day);
+            DayOfWeek dayOfWeek = birthday.DayOfWeek;
+            switch (dayOfWeek) {
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("あなたは日曜日に生まれました");
+                    break;
+
+                case DayOfWeek.Monday:
+                    Console.WriteLine("あなたは月曜日に生まれました");
+                    break;
+
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("あなたは火曜日に生まれました");
+                    break;
+                
+                case DayOfWeek.Wednesday:
+                    Console.WriteLine("あなたは水曜日に生まれました");
+                    break;
+
+                case DayOfWeek.Thursday:
+                    Console.WriteLine("あなたは木曜日に生まれました");
+                    break;
+
+                case DayOfWeek.Friday:
+                    Console.WriteLine("あなたは金曜日に生まれました");
+                    break;
+
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("あなたは土曜日に生まれました");
+                    break;
+            }
+        }
+    }
+}
