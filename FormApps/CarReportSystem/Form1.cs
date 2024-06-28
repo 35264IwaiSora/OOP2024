c2b973e874934b657663267dc2e2b9ff62310a7c
IwaiSora <166583838+35264IwaiSora@users.noreply.github.com>
2024-06-28 10:34:07 +0900

    追加ボタン処理

--- FormApps/CarReportSystem/Form1.cs
+++ FormApps/CarReportSystem/Form1.cs
@@ -11,15 +11,32 @@ namespace CarReportSystem {
         }
 
         private void btAddReport_Click(object sender, EventArgs e) {
-            CarReport carReport = new CarReport { 
+            CarReport carReport = new CarReport {
                 Date = dtpDate.Value,
-            
-            
+                Auther = cbAuther.Text,
+                Maker = GetRadioButtonMaker(),
+                CarName = cbCarName.Text,
+                Report = tbReport.Text,
+                Picture = pbPicture.Image,
             };
             listCarReport.Add(carReport);
 
-
-
+        }
+        //選択されているメーカー名を列挙型で返す
+        private CarReport.MakerGroup GetRadioButtonMaker() {
+            if(rbToyota.Checked) {
+                return CarReport.MakerGroup.トヨタ;
+            }else if(rbNissan.Checked) {
+                return CarReport.MakerGroup.日産;
+            }else if(rbHonda.Checked) {
+                return CarReport.MakerGroup.ホンダ;
+            }else if(rbSubaru.Checked) {
+                return CarReport.MakerGroup.スバル;
+            }else if(rbInport.Checked){
+                return CarReport.MakerGroup.輸入車;
+            }else {
+                return CarReport.MakerGroup.その他;
+            }
         }
     }
 }
