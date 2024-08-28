2ed743ee18d94bf216f2513358903298bbb7086a
IwaiSora <166583838+35264IwaiSora@users.noreply.github.com>
2024-08-28 15:12:18 +0900

    12.1.4完成

--- Chapter12/演習問題/Exercise01/Employee.cs
+++ Chapter12/演習問題/Exercise01/Employee.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Security.Policy;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
 namespace Exercise01 {
     public class Employee {
+        [JsonIgnore]
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime HireDate { get; set; }
--- Chapter12/演習問題/Exercise01/Program.cs
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -4,6 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
@@ -83,7 +86,27 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_4(string file) {
-            
+            var emps = new Employee[] {
+                new Employee {
+                    Id = 123,
+                    Name = "井出　秀行",
+                    HireDate = new DateTime(2001,5,10),
+                },
+                new Employee {
+                    Id = 139,
+                    Name = "大橋　孝二",
+                    HireDate = new DateTime(2004,12,1),
+                },
+            };
+            var options = new JsonSerializerOptions {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                WriteIndented = true,
+            };
+            using (var stream = new FileStream(file, FileMode.Create, FileAccess.Write)) {
+                string jsonString = JsonSerializer.Serialize(emps, options);
+                JsonSerializer.Serialize(stream, emps, options);
+                
+            }
         }
     }
 }
