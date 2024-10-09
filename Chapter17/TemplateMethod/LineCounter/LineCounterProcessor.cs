dbf3a6255e1d6a7a2ed8340f273bdc4959b7f88b
IwaiSora <oibc35264@ojs.ac.jp>
2024-10-09 14:58:15 +0900

    templateメソッドパターン

---
+++ Chapter17/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileProcessor;
+
+namespace LineCounter {
+    class LineCounterProcessor : TextProcessor{
+        string _text = "";
+        private int _count;
+        protected override void Initialize(string fname) {
+           _count = 0;
+        }
+        protected override void Execute(string line) {
+            Console.WriteLine(line.ToUpper());
+            _text += line;
+           _count++;
+        }
+        protected override void Terminate() {
+            Console.WriteLine("{0}行",_count);
+        }
+    }
+}
