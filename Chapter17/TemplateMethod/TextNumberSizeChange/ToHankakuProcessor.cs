ac4ee912aa08fc2e42d829b52b528fbab69c7696
IwaiSora <oibc35264@ojs.ac.jp>
2024-10-16 14:19:50 +0900

    演習問題17-1完成

---
+++ Chapter17/TemplateMethod/TextNumberSizeChange/ToHankakuProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileProcessor;
+
+namespace TextNumberSizeChange {
+    class ToHankakuProcessor : TextProcessor{
+        
+        protected override void Initialize(string fname) {
+           
+        }
+        protected override void Execute(string line) {
+            string output = new string(line.Select(c=>(c>='０'&&c<='９')?(char)(c-'０'+'0'):c).ToArray());
+            Console.WriteLine(output);
+        }
+        protected override void Terminate() {
+            
+        }
+    }
+}
